b7194ce27bb1e12437e55f201d178d13b2726412
Mack Grissom <mcintyregrissom@gmail.com>
2023-05-18 12:14:46 +0630

    added demo pre-api

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { logo } from '../assets'
 const Hero = () => {
     return (
         <header className='w-full flex justify-center items-center flex-col'>
-            <nav className='flex justify-between items-center w-full'>
+            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
 
                 <img src={logo} alt='logo' className='w-28 object-contain' />
 
@@ -15,6 +15,10 @@ const Hero = () => {
                Github
                </button>
         </nav>
+        <h1 className='head_text'> Streamlined Article Summarization<br className='max-md:hidden'/>
+        <span className='orange_gradient'> Using OpenAI GPT-4</span> </h1>
+<h2 className="desc"> Accelerate Learning with Summify: The Open-Source Article Summarizer that Simplifies Complex Content</h2>
+
         </header >
     )
 }
