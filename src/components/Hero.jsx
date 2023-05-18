e84cd1bab7cd9735442a486bf489fa77f638fd21
Mack Grissom <mcintyregrissom@gmail.com>
2023-05-18 18:56:13 +0630

    added CTA

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,16 +8,32 @@ const Hero = () => {
 
                 <img src={logo} alt='logo' className='w-28 object-contain' />
 
-               <button
+               {/* <button
                type="button"
                onClick={() => window.open('https://github.com/mackgrissom')}
                className='black_btn'>
                Github
+               </button> */}
+               <button
+               type="button"
+               onClick={() => window.open('https://github.com/mackgrissom')}
+               className='black_btn'>
+               Support The Developer
                </button>
         </nav>
-        <h1 className='head_text'> Streamlined Article Summarization<br className='max-md:hidden'/>
-        <span className='orange_gradient'> Using OpenAI GPT-4</span> </h1>
-<h2 className="desc"> Accelerate Learning with Summify: The Open-Source Article Summarizer that Simplifies Complex Content</h2>
+        <h1 className='head_text mb-5'> More  <span className=' orange_gradient'> Knowledge, </span> Less Time <br className='max-md:hidden'/> 
+           
+      </h1>
+<h2 className="desc pt-5"> Accelerate Learning with Sumorize: The Open-Source Article & Webpage Summarizer that Simplifies Complex Content Using GPT-4 By OpenAI</h2>
+
+<div className="flex items-center justify-center mt-5">
+      <button className="black_btn mr-4">
+        Join the Waitlist
+      </button>
+      <a href="https://github.com" className="text-blue-500 hover:text-blue-700 font-bold">
+        Contribute on GitHub
+      </a>
+    </div>
 
         </header >
     )
